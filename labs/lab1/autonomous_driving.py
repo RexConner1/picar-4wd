895df8d6ab823b5afdb2978ecf1190661dba8b56
David Orona <oronadavid@gmail.com>
2025-02-21 20:32:21 -0600

    Simplify directory

--- labs/lab1/autonomous_driving.py
+++ labs/lab1/autonomous_driving.py
@@ -5,7 +5,7 @@ import heapq
 import picar_4wd.helpers as defaults
 import picar_4wd.helpers.navigation as car
 import picar_4wd.helpers.visuals as scan
-from labs.lab1.tools import advanced_mapping as mapping, object_detection as detect
+from labs.lab1 import advanced_mapping as mapping, object_detection as detect
 from types import SimpleNamespace
 
 cap = cv2.VideoCapture(0)
